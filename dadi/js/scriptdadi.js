283269e522083a47ec6eccb15ccb643ee99b8d87
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-06-10 00:23:04 +0200

    build: project set-up of dice game

---
+++ dadi/js/scriptdadi.js
@@ -0,0 +1,9 @@
+//prendo gli elementi html che mi servono
+//creo un evento sul bottone per giocare
+//genero dei numeri casuali da 1 a 6 per il pc
+//genero dei numeri casuali da 1 a 6 per l'user
+// creo le condizioni dove
+//se il punteggio dell'user è maggiore del punteggio del pc vince user
+//se il punteggio dell'pc è maggiore del punteggio del user vince pc
+//altrimenti parità
+//stampo il risultato nel mio div result
